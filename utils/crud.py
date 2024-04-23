fb734dee5a72b58c9ebd1dba9c5f0865b942c3bd
martynakaleta <167193864+martynakaleta@users.noreply.github.com>
2024-04-23 14:25:54 +0200

    fead: add create

--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,9 @@
 def read(users: list[dict[str,str]])->None:
     for user in users[1:]:
         print(f'Twój znajomy {user['name']} opulikował {user['post']} posty')
+
+def add_user(lista: list) -> None:
+        user_name = input('podaj imię:')
+        user_surname = input('podaj nazwisko:')
+        user_post = input('podaj ile postów:')
+        lista.append({'name': user_name, 'surname': user_surname, 'post': user_post})
