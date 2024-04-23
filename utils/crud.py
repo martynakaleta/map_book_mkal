85aa15e16343831427ae1909c6882336ba53ac21
martynakaleta <167193864+martynakaleta@users.noreply.github.com>
2024-04-23 17:12:31 +0200

    fead: update was added

--- utils/crud.py
+++ utils/crud.py
@@ -8,11 +8,11 @@ def add_user(lista: list) -> None:
         user_post = input('podaj ile postów:')
         lista.append({'name': user_name, 'surname': user_surname, 'post': user_post})
 
- def search(users: list[dict[str, str]]) -> None:
-         user_name = input('kogo szukasz?')
-            for user in users[1:]:
-                if user['name'] == user_name:
-                    print(f'znaleziono użytkownika {user}')
+def search(users: list[dict[str, str]]) -> None:
+    user_name = input('kogo szukasz?')
+    for user in users[1:]:
+        if user['name'] == user_name:
+            print(f'znaleziono użytkownika {user}')
 
 def remove_user(users: list[dict[str,str]])->None:
     user_name=input('kogo usunąć?')
